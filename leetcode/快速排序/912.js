1b4f80cd6f0855017fabf51aace73ce7896f7a75
12138qzj <2442319903@qq.com>
2020-05-28 23:04:56 +0800

    快速排序

---
+++ leetcode/快速排序/912.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+let nums = [7, 9, 8, -1, 5];
+var sortArray = function(nums) {
+
+    function quick_sort(left, right) {
+        if (left >= right) return;
+        let priv = nums[left];
+        while (left < right) {
+
+            while (left < right && priv <= nums[right]) right--;
+            nums[left] = nums[right];
+            while (left < right && priv >= nums[left]) left++;
+            nums[right] = nums[left];
+
+        }
+        nums[right] = priv;
+        quick_sort(0, left - 1);
+        quick_sort(left + 1, right)
+        return nums;
+    }
+    quick_sort(0, nums.length - 1)
+    console.log(quick_sort(0, nums.length - 1));
+    console.log(nums);
+};
+sortArray(nums)
---
+++ leetcode/快速排序/demo.js
@@ -0,0 +1,9 @@
+/***
+ * 
+ * */
+function partition(nums, left, rigth) {
+    let provit = num[left];
+    while (left < rigth) {
+
+    }
+}
