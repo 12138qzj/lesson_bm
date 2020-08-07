8df5700d512430b333f055a857c0a8c88237fbcf
12138qzj <2442319903@qq.com>
2020-08-07 12:26:07 +0800

    8-7

--- js/草稿文件/4.js
+++ js/草稿文件/4.js
@@ -1,7 +1,7 @@
-// for (let i = 0; i < 5; i++) {
-//     setTimeout(function() {
-//         console.log(i);
-//     }, 1000);
-// }
+for (let i = 0; i < 5; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }, 1000);
+}
 
-// console.log(i);
+console.log(i);
