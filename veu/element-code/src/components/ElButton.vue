7e80a2ed28e60a6e36d57a7e8c01d65d7c31b543
12138qzj <2442319903@qq.com>
2020-08-25 21:17:39 +0800

    vue学习

---
+++ veu/element-code/src/components/ElButton.vue
@@ -0,0 +1,47 @@
+<template>
+<button 
+:class="[round? 'is-round':'' , type?'el-button':'',{
+    'is-circle':circle
+}]"
+@click="handleClick">
+    Button
+    000{{type}}---{{tit}}
+
+
+    <slot></slot>
+</button>
+</template>
+<script>
+export default {
+    // name: 'HelloWorld',
+    data(){
+        return{
+            tit:"hello"
+        }
+    },
+    props: {
+        type: {
+            type:String,
+            // children:String,
+            default:'default'
+        },
+        round:Boolean,
+        circle:Boolean
+    },
+    created(){
+        //生命周期、
+        console.log(this.props,this.tit)
+    },
+    methods:{
+        handleClick:function(evt){
+            this.$emit('click',evt)
+            console.log("数据",evt)
+        }
+    }
+}
+
+</script>
+<style scoped>
+
+</style>
+
---
+++ veu/readme.md
@@ -0,0 +1,16 @@
+vue create project-name
+
+    使用vue2.0 vue init webpack project-name
+
+    PWA 渐进式网页应用
+    - Router Vuex 全家桶
+    - css Pre-processors stylus vw autoprefixer
+        编译 sass/stylus 插件 autoprefixer 前缀
+        px-vw- 尺寸的统一 cssnext
+        
+    - linter 代码风格确保文件 ; "" '' tab ESlint 代码风格 保证代码风格是一样的
+        不用的变量不要定义
+        代码对齐
+        "" '' 要统一
+        js 的结尾不加;
+    - unit Testing E2E 测试
