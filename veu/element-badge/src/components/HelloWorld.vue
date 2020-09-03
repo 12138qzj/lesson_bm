531f8d21fcfa75537fb98b7cdcafeb4d12438fdb
12138qzj <2442319903@qq.com>
2020-09-03 22:16:58 +0800

    vue-element-Badge

---
+++ veu/element-badge/src/components/HelloWorld.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="hello">
+    <id-badge :value="12" :max="10" class="item" :hidden="isHidden">
+      <el-button size="small">评论</el-button>
+    </id-badge>
+
+    <el-badge :value="200" :max="99" class="item">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+  </div>
+</template>
+
+<script>
+
+import IdBadge from '@/components/IdBadge'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      isHidden:false
+    }
+  },
+  meunted(){
+    setTimeout(()=>{
+      this.isHidden=true;
+    },2000)
+  },
+  components:{
+    IdBadge
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ veu/element-badge/src/components/IdBadge.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="el-badge">
+        <solt>   </solt>    
+        <transition name="el-zoom-in-center">
+
+            <sup
+            v-show="!hidden"
+            v-text="content" 
+            class="el-badge__content"
+            >
+            </sup>    
+        </transition>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"IdBadge",
+        props:{
+            value:[String,Number],
+            max:Number,
+            isDot:Boolean,
+            hidden:Boolean,
+            type:{
+                type:String,
+                validator(val){
+                    return ['primary','success','info','danger'].indexOf(val)>-1
+                }
+            }
+        },
+        computed:{
+            content(){
+                if(this.isDot)return;
+                const value=this.value;
+                const max=this.max;
+                if(typeof value==='number'&& typeof max==="number"){
+                    return value>max?`${max}+`:`${value}`;
+                }
+                return value;
+            }
+        }
+        
+    }
+</script>
+
+<style >
+
+</style>
